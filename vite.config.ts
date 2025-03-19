1e1c96779f99a292c81c2c965a6cb295bac5a2d5
sargsalbert <sargsyan.abet@gmail.com>
2025-03-19 16:58:09 +0400

    Add CI/CD, EsLint, Prettier, Vitest and Vite optimizations

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config"; 
+import react from "@vitejs/plugin-react";
+import viteCompression from "vite-plugin-compression";
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteCompression({ algorithm: "gzip" }), 
+  ],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+  build: {
+    minify: 'terser', 
+    terserOptions: {
+      compress: {
+        drop_console: true, 
+        drop_debugger: true,
+      },
+      output: {
+        comments: false, 
+      },
+    },
+    rollupOptions: {
+      treeshake: true, 
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; 
+          }
+        },
+      },
+    },
+  },
+  esbuild: {
+    drop: ['console', 'debugger'],
+  },
+});
